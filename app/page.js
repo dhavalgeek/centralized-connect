946f641966ac691cba4cc1bf35c47facb81e05b0
Dhaval Makwana <dhavalmgeek435@gmail.com>
2025-04-10 15:47:11 +0530

    Added shared components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import { HelloWorld } from "@/centralized/shared";
-import Image from "next/image";
+import { HelloWorld } from "@/centralized/components/hello-world";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <HelloWorld />
-    </div>
+    <>
+      <HelloWorld description="▶ USED or Connected Centralized component" />
+    </>
   );
 }
